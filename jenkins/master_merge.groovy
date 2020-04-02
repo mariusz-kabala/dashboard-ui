c05cd612f54a59dceb543bc366dbca6a687310d8
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-04-02 12:34:05 +0200

    ci: pipeline fixes

--- jenkins/master_merge.groovy
+++ jenkins/master_merge.groovy
@@ -5,10 +5,11 @@ pipeline {
     agent { docker { image 'docker-registry.kabala.tech/node12-with-git:latest' } }
 
     environment {
-        GITHUB_API_KEY = credentials('jenkins-github-accesstoken')
+        GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no"
         CI = 'true'
         AWS_ACCESS_KEY_ID = credentials('SCALEWAY_S3_ACCESS_KEY')
         AWS_SECRET_ACCESS_KEY = credentials('SCALEWAY_S3_ACCESS_SECRET_KEY') 
+        GH_TOKEN = credentials('jenkins-github-accesstoken')
     }
 
     stages {
