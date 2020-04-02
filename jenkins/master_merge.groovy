7dd23a9c62ed9f9f6dd6fb81fb4d2778ea1c3faf
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-04-02 12:23:04 +0200

    ci: pipeline fixes

--- jenkins/master_merge.groovy
+++ jenkins/master_merge.groovy
@@ -27,6 +27,9 @@ pipeline {
                         println "GIT BRANCH not detected"
                     }
 
+                    sh 'git config user.name "jenkins-kabala.tech"'
+                    sh 'git config user.email "jenkins@kabala.tech"'
+
                     if (!branch) {
                         error "GIT branch to process not found"
                     }
