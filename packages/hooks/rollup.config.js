e676e389fdb0b44ce064ac0c295cd55340e5018e
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-04-09 08:22:58 +0200

    feat: add custom rollup config for hooks package

---
+++ packages/hooks/rollup.config.js
@@ -0,0 +1,43 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import builtins from 'rollup-plugin-node-builtins'
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: `./src/index.tsx`,
+  output: [
+    {
+      file: `./dist/index.esm.js`,
+      format: 'esm',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+      },
+    },
+    {
+      file: `./dist/index.js`,
+      format: 'cjs',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+      },
+    },
+  ],
+  external: [
+    'react',
+    'react-dom',
+    'react-router-dom',
+    'classnames',
+    'reactstrap',
+    'recharts',
+    'react-smooth-scrollbar',
+  ],
+  plugins: [
+    typescript(),
+    resolve({
+      preferBuiltins: false,
+    }),
+    builtins(),
+    commonjs(),
+  ],
+}
